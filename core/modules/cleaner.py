1bef10d138a1a4947f142f3723dc959af0acc2fa
Akshat Nigam <nigamakshat2406@gmail.com>
2026-01-27 12:15:42 +0530

    Added cleaner file

---
+++ core/modules/cleaner.py
@@ -0,0 +1,24 @@
+import re
+
+def extract_emails(text):
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    emails = re.findall(email_pattern, text, flags=0)
+    return emails
+
+def extract_phone(text):
+    phone_pattern = r'(?:\+91|0)?\s?\d{10}'
+    phone_no = re.findall(phone_pattern, text, flags=0)
+    return phone_no
+
+
+def cleaner_text(raw_file):
+    
+    text = raw_file.lower()
+    
+    text = text.replace('\n' , " ")
+    
+    text = re.sub(r"[^\w\s]" , "", text)
+
+    text = re.sub(r"\s+", " " , text)
+    
+    return text
---
+++ tests/test_extractor.py
@@ -0,0 +1,40 @@
+import PyPDF2
+import os
+
+# --- Your Extraction Function ---
+def extract_text_from_pdf(file_path): 
+    text = ""
+    try:
+        with open(file_path, 'rb') as file: 
+            reader = PyPDF2.PdfReader(file)
+            
+            for page in reader.pages:   
+                page_text = page.extract_text() 
+                if page_text:       
+                    text += page_text + "\n" 
+
+        if not text.strip():  
+            print("No text found in the PDF.")
+        
+        return text
+    
+    except FileNotFoundError:
+        return "Error: The file was not found at the specified path."
+    except Exception as e:
+        return f"Error processing PDF: {str(e)}"
+
+
+
+# --- Execution Block ---
+if __name__ == "__main__":
+    import os
+    
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    test_filename = os.path.join(script_directory, "my_notes.pdf")
+    
+    print(f"\n--- Extracting Text from: {test_filename} ---")
+    
+    # 3. Run the extractor
+    result = extract_text_from_pdf(test_filename)
+    print(result)
+    
